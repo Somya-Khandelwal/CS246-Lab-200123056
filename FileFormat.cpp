b9feb123c4486143f7d79f038f4a66a174f8e576
Somya-Khandelwal <somyakhwal@gmail.com>
2022-04-18 09:35:50 +0530

    Update

--- FileFormat.cpp
+++ FileFormat.cpp
@@ -4,85 +4,88 @@ using namespace std;
 
 int PAGE_SIZE;
 
-class Page 
+class Page
 {
 public:
     int used;
     vector<int> records;
     Page *next, *prev;
 
-    Page() 
+    Page()
     {
         used = 4;
         next = nullptr;
         prev = nullptr;
     }
 
-    int empty_space() 
+    int empty_space()
     {
         return PAGE_SIZE - 4 - used;
     }
 
-    bool insert(int sz, int pk) 
+    bool insert(int sz, int pk)
     {
-        if (empty_space() >= sz+4) 
+        if (empty_space() >= sz + 4)
         {
-            used += sz+4;
+            used += sz + 4;
             records.push_back(pk);
             return true;
         }
         return false;
     }
 
-    int search(int pk) 
+    int search(int pk)
     {
-        for (int i = 0; i < records.size(); i++) 
+        for (int i = 0; i < records.size(); i++)
         {
-            if (records[i] == pk) return i;
+            if (records[i] == pk)
+                return i;
         }
         return -1;
     }
 };
 
-class File 
+class File
 {
 public:
-    Page* first;
+    Page *first;
     int pages;
 
-    File() 
+    File()
     {
         first = nullptr;
         pages = 0;
     }
 
-    void insert(int sz, int pk) 
+    void insert(int sz, int pk)
     {
-        Page* curr = first, *last = nullptr;
-        while (true) 
+        Page *curr = first, *last = nullptr;
+        while (true)
         {
-            if (!curr) 
+            if (!curr)
             {
                 curr = new Page;
-                if (!first) first = curr;
+                if (!first)
+                    first = curr;
                 pages++;
-                if (last) 
+                if (last)
                 {
                     last->next = curr;
                     curr->prev = last;
                 }
             }
-            if (curr->insert(sz, pk)) return;
+            if (curr->insert(sz, pk))
+                return;
             last = curr;
             curr = curr->next;
         }
     }
 
-    void display_status() 
+    void display_status()
     {
         cout << pages << " ";
-        Page* curr = first;
-        while(curr) 
+        Page *curr = first;
+        while (curr)
         {
             cout << curr->records.size() << " ";
             curr = curr->next;
@@ -90,13 +93,14 @@ public:
         cout << endl;
     }
 
-    pair<int, int> search(int pk) 
+    pair<int, int> search(int pk)
     {
         int page_number = 0;
-        Page* curr = first;
-        while (curr) 
+        Page *curr = first;
+        while (curr)
         {
-            if (curr->search(pk) >= 0) return {page_number, curr->search(pk)};
+            if (curr->search(pk) >= 0)
+                return {page_number, curr->search(pk)};
             curr = curr->next;
             page_number++;
         }
@@ -104,7 +108,7 @@ public:
     }
 };
 
-int main() 
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -116,28 +120,29 @@ int main()
 
     int type, sz, pk;
 
-    while(true) 
+    while (true)
     {
         cin >> type;
-        if (type == 1) 
+        if (type == 1)
         {
             // INSERT
             cin >> sz >> pk;
             file.insert(sz, pk);
         }
-        else if (type == 2) 
-        { 
+        else if (type == 2)
+        {
             // STATUS
             file.display_status();
         }
-        else if (type == 3) 
-        { 
+        else if (type == 3)
+        {
             // SEARCH
             cin >> pk;
             pair<int, int> ans = file.search(pk);
             cout << ans.first << " " << ans.second << endl;
         }
-        else break;
+        else
+            break;
     }
 
     return 0;
